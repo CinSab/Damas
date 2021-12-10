7849064810419341eaec9e7a249346ee6a5a8c5e
jhenry <jhenrymamani51@gmail.com>
2021-12-10 17:56:14 -0300

    cambio en tablero

--- src/TableroPanel.java
+++ src/TableroPanel.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+//estas
 public class TableroPanel extends JPanel {
     private final Image backgroundImage;
 

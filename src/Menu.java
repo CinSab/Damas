5bc8e7730c8aa450da486b846995c5b378f5dc12
jhenry <jhenrymamani51@gmail.com>
2021-12-11 20:15:26 -0300

    Movimiento de los peones (beta)

--- src/Menu.java
+++ src/Menu.java
@@ -1,19 +1,21 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.IOException;
 
-public class Menu extends JFrame{
+public class Menu extends JFrame {
 
     public Menu() throws IOException {
         super("Menu");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400,300);
+        setSize(400, 300);
         Container cp = getContentPane();
         JPanel menu = new JPanel();
-        GridLayout gl = new GridLayout(4,1,5,5);
+        GridLayout gl = new GridLayout(4, 1, 5, 5);
         JPanel jugar = new JPanel();
         JPanel cargarpartida = new JPanel();
         JPanel verreglas = new JPanel();
@@ -22,14 +24,35 @@ public class Menu extends JFrame{
         cargarpartida.setLayout(new FlowLayout());
         verreglas.setLayout(new FlowLayout());
         salir.setLayout(new FlowLayout());
-        jugar.add(new JButton("jugar"));
+        JButton jugar2 = new JButton("jugar");
+        jugar2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                VentanaDamas ventana = null;
+                try {
+                    ventana = new VentanaDamas();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                TableroPanel tablero = null;
+                try {
+                    tablero = new TableroPanel();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                ventana.add(tablero);
+                ventana.setVisible(true);
+            }
+
+        });
+        jugar.add(jugar2);
         cargarpartida.add(new JButton("Cargar Partida"));
         verreglas.add(new JButton("Ver reglas"));
         salir.add(new JButton("salir"));
-        getContentPane().add(jugar,BorderLayout.CENTER);
-        getContentPane().add(cargarpartida,BorderLayout.CENTER);
-        getContentPane().add(verreglas,BorderLayout.CENTER);
-        getContentPane().add(salir,BorderLayout.CENTER);
+        getContentPane().add(jugar, BorderLayout.CENTER);
+        getContentPane().add(cargarpartida, BorderLayout.CENTER);
+        getContentPane().add(verreglas, BorderLayout.CENTER);
+        getContentPane().add(salir, BorderLayout.CENTER);
 
         menu.setLayout(gl);
         menu.add(jugar);
@@ -37,22 +60,15 @@ public class Menu extends JFrame{
         menu.add(verreglas);
         menu.add(salir);
         setResizable(false);
-        getContentPane().add(menu,BorderLayout.CENTER);
+        getContentPane().add(menu, BorderLayout.CENTER);
         cp.add(menu);
-
-        addMouseListener(new MouseListener() {
-            @Override public void mousePressed(MouseEvent e) { opcion(e.getButton()); }
-            @Override public void mouseClicked(MouseEvent e) {}
-            @Override public void mouseReleased(MouseEvent e) {}
-            @Override public void mouseEntered(MouseEvent e) {}
-            @Override public void mouseExited(MouseEvent e) {}
-        });
     }
 
-    public void opcion(int boton){
+    public void opcion(int boton) {
         System.out.println(boton);
     }
-    public void pintarmenu(Graphics g){
+
+    public void pintarmenu(Graphics g) {
 
     }
 }
--- src/VentanaDamas.java
+++ src/VentanaDamas.java
@@ -21,6 +21,4 @@ public class VentanaDamas  extends JFrame {
         pack();
         setResizable(false);
     }
-
-
 }

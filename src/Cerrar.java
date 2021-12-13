78c0f37e7f7b35caa74cc2b620958908e5fb3795
jhenry <jhenrymamani51@gmail.com>
2021-12-13 00:36:38 -0300

    ahora cerrar funciona

---
+++ src/Cerrar.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Cerrar implements ActionListener {
+
+    static boolean apretado = false;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        VentanaDamas ventana = new VentanaDamas();
+        apretado = true;
+        if (apretado) {
+            Main.jugar();
+        }
+    }
+}

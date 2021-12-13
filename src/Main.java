a59ac04242a0e391e06cdfde1983b3317ce6ffb6
jhenry <jhenrymamani51@gmail.com>
2021-12-13 00:36:57 -0300

    se agrego metodos estaticos

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
-public class Main {
+public class Main extends Cerrar {
+    private static Menu menu;
+
     public static void main(String[] args) throws IOException {
-        Menu menu = new Menu();
+        menu = new Menu();
         menu.setVisible(true);
+        menu.jugar2.addActionListener(new Cerrar());
+    }
+
+    public static void jugar() {
+        menu.setVisible(false);
     }
 }

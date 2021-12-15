a84ecd47689a1d359a5adf356409d3210148ca1f
jhenry <jhenrymamani51@gmail.com>
2021-12-15 18:23:57 -0300

    clase cerrar en main para cerrar la ventana al abrir la otra

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
-public class Main extends Cerrar {
+public class Main {
     private static Menu menu;
-
+    public static class Cerrar implements ActionListener {
+        static boolean apretado = false;
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            VentanaDamas ventana = new VentanaDamas();
+            apretado = true;
+            if (apretado) {
+                Main.jugar();
+            }
+        }
+    }
     public static void main(String[] args) throws IOException {
         menu = new Menu();
         menu.setVisible(true);

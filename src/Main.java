cbb91256d3f8128390b857b012b8fcc922730c13
jhenry <jhenrymamani51@gmail.com>
2021-12-15 22:15:19 -0300

    la clase "cerrar" se puso dentro del main con otro nombre

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 
 public class Main {
     private static Menu menu;
-    public static class Cerrar implements ActionListener {
+    public static class AbrirventanaDamas implements ActionListener {
+        boolean cargarpartida;
+        public AbrirventanaDamas(boolean cargarpartida){
+            this.cargarpartida=cargarpartida;
+        }
         static boolean apretado = false;
         @Override
         public void actionPerformed(ActionEvent e) {
-            VentanaDamas ventana = new VentanaDamas();
+            VentanaDamas ventana = new VentanaDamas(cargarpartida);
             apretado = true;
             if (apretado) {
                 Main.jugar();
@@ -18,7 +22,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         menu = new Menu();
         menu.setVisible(true);
-        menu.jugar2.addActionListener(new Cerrar());
+        menu.jugar2.addActionListener(new AbrirventanaDamas(false));
+        menu.cargarpartida1.addActionListener(new AbrirventanaDamas(true));
     }
 
     public static void jugar() {

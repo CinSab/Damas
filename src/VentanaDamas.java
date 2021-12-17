1346852cb0428185707232f59a1302f3af2013e4
jhenry <jhenrymamani51@gmail.com>
2021-12-17 18:44:26 -0300

    3 turnos de la dama? buscar error

--- src/VentanaDamas.java
+++ src/VentanaDamas.java
@@ -215,6 +215,7 @@ public class VentanaDamas extends JPanel implements MouseListener {
         }
     }
 
+
     public void cambiarTurno() {
         if (jugadorActual == PEONROJO)
             jugadorActual = PEONBLANCO;
